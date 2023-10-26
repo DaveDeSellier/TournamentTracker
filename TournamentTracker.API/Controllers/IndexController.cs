01c0e0d9f5b395e2eccde18bde4fbe4a59e2a1ff
Dave <davedesellier@gmail.com>
2023-10-26 18:36:54 -0400

    Added home controller

---
+++ TournamentTracker.API/Controllers/IndexController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentTracker.Core.Interfaces;
+using TournamentTracker.Core.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TournamentTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ITournament _tournamentService;
+        public HomeController(ITournament tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<List<Tournament>>> Get()
+        {
+
+            var tournaments = await _tournamentService.List();
+
+            if (tournaments == null)
+            {
+                return NotFound();
+            }
+
+            return tournaments;
+
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet("id")]
+        public async Task<ActionResult<Tournament>> GetTournamentById(int id)
+        {
+
+            var tournament = await _tournamentService.GetById(id);
+
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+
+            return tournament;
+        }
+
+    }
+}
--- TournamentTracker.API/Program.cs
+++ TournamentTracker.API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TournamentTracker.Core;
+using TournamentTracker.Core.Interfaces;
+using TournamentTracker.Infrastructure;
+using TournamentTracker.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TournamentTrackerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConn")));
+builder.Services.AddTransient<ITournament, TournamentService>();
+builder.Services.AddTransient<TournamentLogic>();
 
 var app = builder.Build();
 
