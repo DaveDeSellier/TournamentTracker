f02a96f161d6557ec83c375aaccd00e5e1d19c4a
Dave <davedesellier@gmail.com>
2023-10-27 13:04:26 -0400

    Updated home page to fetch tournaments

--- TournamentTracker.React/src/pages/Home.tsx
+++ TournamentTracker.React/src/pages/Home.tsx
@@ -1,36 +1,73 @@
-import { MouseEventHandler } from "react"
+import GetTournaments, {
+    ITournamentModel,
+    ITournaments,
+} from "../services/TournamentService";
+import React, { useEffect, useState } from "react";
+
+function TournamentList({ tournaments }: ITournaments) {
+    if (tournaments === null) {
+        return <></>;
+    }
+
+    const listItems = tournaments?.map((tournament) => (
+        <option>{tournament.tournamentName}</option>
+    ));
+    return listItems;
+}
 
 export default function Home() {
+    const [data, setData] = useState<ITournamentModel[]>(
+        new Array<ITournamentModel>()
+    );
 
-  const handleLoadTournament = (e : React.MouseEvent<HTMLButtonElement>) => {
+    const fetchData = async () => {
+        const result = await GetTournaments("api/Home");
+        setData(result);
+    };
 
-  }
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  const handleCreateTournament = (e : React.MouseEvent<HTMLButtonElement>) => {
+    const handleLoadTournament = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-  }
+    const handleCreateTournament = (
+        e: React.MouseEvent<HTMLButtonElement>
+    ) => {};
 
-
-  return (
-    <>
+    return (
+        <>
             {/* @if(isVisible){
                 <Alert IsVisible=@isVisible ErrorMessage=@ErrorMessage />
             } */}
-          <div className="d-flex vh-100">
-            <div className="d-flex w-100 m-auto align-items-center flex-column">
+            <div className="d-flex p-5 m-lg-5 align-items-center flex-column">
                 <h1>Tournament Dashboard</h1>
                 <div className="">
-                        <label>Load Existing Tournament</label>
-                        <select className="form-select">
-                          
-                        </select>
+                    <label>Load Existing Tournament</label>
+                    <select className="form-select">
+                        <option defaultValue="-- select an option --">
+                            -- select an option --
+                        </option>
+                        <TournamentList tournaments={data} />
+                    </select>
                 </div>
                 <div className="d-flex flex-column gap-2 mt-4">
-                    <button onClick={(e) => handleLoadTournament(e)} className="btn btn-secondary" type="button">Load Tournament</button>
-                    <button onClick={(e) => handleCreateTournament(e)} className="btn btn-lg btn-primary" type="button">Create Tournament</button>
+                    <button
+                        onClick={(e) => handleLoadTournament(e)}
+                        className="btn btn-secondary"
+                        type="button"
+                    >
+                        Load Tournament
+                    </button>
+                    <button
+                        onClick={(e) => handleCreateTournament(e)}
+                        className="btn btn-lg btn-primary"
+                        type="button"
+                    >
+                        Create Tournament
+                    </button>
                 </div>
             </div>
-          </div> 
-    </>
-  )
+        </>
+    );
 }
