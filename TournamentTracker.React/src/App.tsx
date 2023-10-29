bff095a08cd3fa890d44016dab7daa9b060012d4
Dave <davedesellier@gmail.com>
2023-10-29 16:38:38 -0400

    Updated app to use router

--- TournamentTracker.React/src/App.tsx
+++ TournamentTracker.React/src/App.tsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import CreateTournament from "./pages/CreateTournament";
 
 function App() {
     return (
         <>
-            <Home />
+            <Routes>
+                <Route index element={<Home />} />
+                <Route
+                    path="/createtournament"
+                    element={<CreateTournament />}
+                />
+
+                {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+                {/* </Routes><Route path="*" element={<NoMatch />} /> */}
+            </Routes>
         </>
     );
 }
