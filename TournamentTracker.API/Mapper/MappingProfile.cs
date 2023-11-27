1d3ac54cfe6c9ed978c608c374a8fc70d9c65301
Dave <davedesellier@gmail.com>
2023-11-27 09:29:34 -0500

    Added mapping profile

---
+++ TournamentTracker.API/Mapper/MappingProfile.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using TournamentTracker.API.Models;
+using TournamentTracker.Core.Models;
+
+namespace TournamentTracker.API.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Tournament, TournamentResource>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.EntryFee, opt => opt.MapFrom(src => src.EntryFee))
+                .ForMember(dest => dest.TournamentName, opt => opt.MapFrom(src => src.TournamentName))
+                .ForMember(dest => dest.TournamentPrizes, opt => opt.MapFrom(src => src.TournamentPrizes))
+                .ForMember(dest => dest.TournamentEntries, opt => opt.MapFrom(src => src.TournamentEntries))
+                .ForMember(dest => dest.Matchups, opt => opt.MapFrom(src => src.Matchups))
+                .ReverseMap();
+            //.ForMember(dest => dest.Self, opt => opt.MapFrom(src => Link.To(nameof(Controllers.HomeController.GetTournamentById), new { Id = src.Id })));
+
+            CreateMap<Tournament, PartialTournamentResource>()
+                .ReverseMap();
+
+            CreateMap<TournamentPrize, TournamentPrizeResource>();
+
+            CreateMap<Prize, PrizeResource>()
+                .ReverseMap();
+
+            CreateMap<Team, TeamResource>()
+                .ReverseMap();
+
+            CreateMap<TeamMember, TeamMemberResource>()
+                .ReverseMap();
+
+            CreateMap<TournamentEntry, TournamentEntryResource>()
+                .ReverseMap();
+
+            CreateMap<Matchup, MatchupResource>()
+                .ReverseMap();
+
+            CreateMap<MatchupEntry, MatchupResource>()
+                .ReverseMap();
+
+            CreateMap<Person, PersonResource>()
+                .ReverseMap();
+
+        }
+    }
+}
