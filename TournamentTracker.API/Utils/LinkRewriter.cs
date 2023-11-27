75f98ceb5da2446a22058e912aa41ad66c33c893
Dave <davedesellier@gmail.com>
2023-11-27 09:30:11 -0500

    Added link rewriter class

---
+++ TournamentTracker.API/Utils/LinkRewriter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentTracker.API.Models;
+
+namespace TournamentTracker.API.Utils
+{
+    public class LinkRewriter
+    {
+        private readonly IUrlHelper _urlHelper;
+        public LinkRewriter(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        public Link? Rewrite(Link original)
+        {
+            if (original == null) return null;
+
+            return new Link
+            {
+                Href = _urlHelper.Link(original.RouteName, original.RouteValues)
+            };
+
+        }
+    }
+}
